5dca2434bd26b74b750de51a0badc162f8dd222e
Anag0es <analuisa.goes2004@gmail.com>
2024-03-22 20:49:49 -0300

    feat: auth + registro do usuario

--- authentication.py
+++ authentication.py
@@ -1,4 +1,12 @@
+from fastapi import HTTPException
+from grpc import Status
 from passlib.context import CryptContext
+from models import User
+from config import config_credential
+
+import jwt
+
+from datetime import datetime, timedelta
 
 # pwd -> password
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
@@ -7,5 +15,45 @@ pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 def get_hashed_password(password):
     return pwd_context.hash(password)
 
+async def very_token(token : str):
+    try:
+        payload = jwt.decode(token, config_credential['SECRET'], algorithms=["HS256"])
+        user = await User.get(id=payload.get('id'))
+    except:
+        raise HTTPException(
+            status_code = Status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid token",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+        
+    return user
+
+async def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+async def autheticate_user(username, password):
+    user = await User.get(username=username)
+    
+    if user and verify_password(password, user.password):
+        return user
+    return "Invalid user"
 
 
+async def token_generator(username : str, password : str):
+    user = await autheticate_user(username, password)
+    if not user:
+        raise HTTPException(
+            status_code = Status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        ) 
+        
+    token_data = {
+        "id": user.id,
+        "username": user.username,
+        "exp": datetime.utcnow() + timedelta(hours=1) # token expira em 1 hora
+    }
+    
+    token = jwt.encode(token_data, config_credential['SECRET'], algorithm="HS256")
+    
+    return token
--- main.py
+++ main.py
@@ -1,15 +1,83 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, requests, HTTPException, status
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from tortoise import models
 from tortoise.contrib.fastapi import register_tortoise
-from models import *
-from authentication import (get_hashed_password)
+from models import User, Business, Product
+from models import user_pydantic, user_pydanticIn, business_pydantic
+
+# autenticação
+from authentication import token_generator, get_hashed_password, config_credential
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+# template
+from fastapi.templating import Jinja2Templates
+from jwt import PyJWTError
+import jwt
+from config import config_credential
 
 # Importando o sinal de tortoise
 from tortoise.signals import post_save
 from typing import List, Optional, Type
 from tortoise import BaseDBAsyncClient
 
+# response class
+from fastapi.responses import HTMLResponse
+
+
+
 # Criando a instância do FastAPI
 app = FastAPI()
+
+# Criando a instância do OAuth2
+oath2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+
+@app.post('/token')
+async def generate_token(request_form: OAuth2PasswordRequestForm = Depends()):
+    token = await token_generator(request_form.username, request_form.password)    
+    return {'acess_token': token, "token_type": "bearer"}
+
+
+async def get_current_user(token: str = Depends(oath2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, config_credential['SECRET'], algorithms=["HS256"])
+        user_id: str = payload.get("id")
+        
+        if user_id is None:
+            raise credentials_exception
+        
+    except PyJWTError:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid token",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+        
+    user = await User.get(id=user_id)
+    return user
+
+
+
+@app.post('/user/me')
+async def user_login(user: user_pydanticIn = Depends(get_current_user)):
+    business = await Business.get(owner=user)
+    return {
+        'status': 'ok',
+        'data': {
+            'username': user.username,
+            'email' : user.email,
+            'verify': user.is_verify,
+            'joined_date': user.joined_date.strftime('%b-%d-%Y'),
+        } 
+    }
 
 
 # criar um negócio para cada usuário registrado
